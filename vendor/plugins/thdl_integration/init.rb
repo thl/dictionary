9dbebf5a5175ee07f597bb646298a6704f301f39
Andres Montano <amontano@virginia.edu>
2010-01-28 16:12:10 -0600

    Deleting thdl_integration plugin.

--- vendor/plugins/thdl_integration/init.rb
+++
@@ -1,8 +0,0 @@
-# Include hook code here
-require 'open-uri'
-#require 'open_uri_patch' if ENV['RAILS_ENV']!='development'
-require 'hpricot'
-require 'helpers/frame_helper'
-require 'helpers/thdl_integration_helper'
-ActionView::Base.send :include, ThdlIntegrationHelper
-ActionView::Base.send :include, FrameHelper
--- vendor/plugins/thdl_integration/tasks/thdl_integration_tasks.rake
+++
@@ -1,4 +0,0 @@
-# desc "Explaining what the task does"
-# task :thdl_integration do
-#   # Task goes here
-# end
