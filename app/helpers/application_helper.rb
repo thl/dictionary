dcfc0a3c0fb586b9c9fb8595653fa0d9f7fe8636
Heiser Mazariegos <hm5u@virginia.edu>
2010-03-23 02:15:00 -0600

    modified tinymce_if_needed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -220,7 +220,7 @@ module ApplicationHelper
     end
 
     def javascript_files
-      super + ['in_place_select_editor', 'modalbox','menu', 'menu_items', 'menu_tpl', 'tiny_mce/tiny_mce']
+      super + ['in_place_select_editor', 'modalbox','menu', 'menu_items', 'menu_tpl', 'tiny_mce/tiny_mce_src']
     end
     
     def side_column_links
