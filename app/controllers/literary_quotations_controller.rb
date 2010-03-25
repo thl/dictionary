ae83528d4e8c9274d984919f696435dd929adeae
Heiser Mazariegos <hm5u@virginia.edu>
2010-03-25 00:31:37 -0600

    added tinymce to literary_quotations

--- app/controllers/literary_quotations_controller.rb
+++ app/controllers/literary_quotations_controller.rb
@@ -750,4 +750,164 @@ class LiteraryQuotationsController < ApplicationController
            render_action 'list'
          end
   end
+  
+  
+  def update_analytical_note
+    debugger
+      @literary_quotation = LiteraryQuotation.find(params[:literary_quotation][:id])
+      if @literary_quotation.created_by == nil or @literary_quotation.created_by == ""
+             @literary_quotation.created_by = session[:user].login
+             @literary_quotation.created_at = Time.now
+      end
+      if session[:user] != nil
+             @literary_quotation.updated_by = session[:user].login
+      end
+      @literary_quotation.updated_at = Time.now
+      if @literary_quotation.update_history == nil
+        @literary_quotation.update_history = session[:user].login + " ["+Time.now.to_s+"]
+       "
+      else
+        @literary_quotation.update_history += session[:user].login + " ["+Time.now.to_s+"]
+       "
+      end
+      respond_to do |format|
+        if @literary_quotation.update_attributes(params[:literary_quotation])
+          format.html do
+            render :partial => 'shared/tinymce_field_show', :locals => {:t => @literary_quotation, :divsuffix => "_anotediv"}
+          end
+        else
+           #redirect_to :action => 'index_edit'
+           #redirect_to :action => 'public_edit', :id => @literary_quotation
+        end
+      end
+  end
+
+  def analytical_note_show
+      @literary_quotation = LiteraryQuotation.find(params[:id])
+      render :partial => "shared/tinymce_field_show", :locals => {:t => @literary_quotation, :divsuffix => "_anotediv"}
+  end
+
+  def analytical_note_edit
+      @literary_quotation = LiteraryQuotation.find(params[:id])
+      render :partial => "shared/tinymce_field_edit", :locals => {:t => @literary_quotation, :divsuffix => "_anotediv"}
+  end
+
+  def update_image_description
+    debugger
+      @literary_quotation = LiteraryQuotation.find(params[:literary_quotation][:id])
+      if @literary_quotation.created_by == nil or @literary_quotation.created_by == ""
+        @literary_quotation.created_by = session[:user].login
+        @literary_quotation.created_at = Time.now
+      end
+      if session[:user] != nil
+        @literary_quotation.updated_by = session[:user].login
+      end
+      @literary_quotation.updated_at = Time.now
+      if @literary_quotation.update_history == nil
+        @literary_quotation.update_history = session[:user].login + " ["+Time.now.to_s+"]
+  "
+      else
+        @literary_quotation.update_history += session[:user].login + " ["+Time.now.to_s+"]
+  "
+      end    
+      respond_to do |format|
+        if @literary_quotation.update_attributes(params[:literary_quotation])
+          format.html do
+            render :partial => 'shared/tinymce_field_show', :locals => {:t => @literary_quotation, :divsuffix => "_imagedescdiv"}
+          end
+        else
+          #redirect_to :action => 'index_edit'
+          #redirect_to :action => 'public_edit', :id => @literary_quotation
+        end
+      end
+  end
+
+    def image_description_show
+      @literary_quotation = LiteraryQuotation.find(params[:id])
+      render :partial => "shared/tinymce_field_show", :locals => {:t => @literary_quotation, :divsuffix => "_imagedescdiv"}
+    end
+
+    def image_description_edit
+      @literary_quotation = LiteraryQuotation.find(params[:id])
+      render :partial => "shared/tinymce_field_edit", :locals => {:t => @literary_quotation, :divsuffix => "_imagedescdiv"}
+    end 
+
+    def update_audio_description
+      @literary_quotation = LiteraryQuotation.find(params[:literary_quotation][:id])
+      if @literary_quotation.created_by == nil or @literary_quotation.created_by == ""
+        @literary_quotation.created_by = session[:user].login
+        @literary_quotation.created_at = Time.now
+      end
+      if session[:user] != nil
+        @literary_quotation.updated_by = session[:user].login
+      end
+      @literary_quotation.updated_at = Time.now
+      if @literary_quotation.update_history == nil
+        @literary_quotation.update_history = session[:user].login + " ["+Time.now.to_s+"]
+  "
+      else
+        @literary_quotation.update_history += session[:user].login + " ["+Time.now.to_s+"]
+  "
+      end    
+      respond_to do |format|
+        if @literary_quotation.update_attributes(params[:literary_quotation])
+          format.html do
+            render :partial => 'shared/tinymce_field_show', :locals => {:t => @literary_quotation, :divsuffix => "_audiodescdiv"}
+          end
+        else
+          #redirect_to :action => 'index_edit'
+          #redirect_to :action => 'public_edit', :id => @literary_quotation
+        end
+      end
+    end
+
+    def audio_description_show
+      @literary_quotation = LiteraryQuotation.find(params[:id])
+      render :partial => "shared/tinymce_field_show", :locals => {:t => @literary_quotation, :divsuffix => "_audiodescdiv"}
+    end
+
+    def audio_description_edit
+      @literary_quotation = LiteraryQuotation.find(params[:id])
+      render :partial => "shared/tinymce_field_edit", :locals => {:t => @literary_quotation, :divsuffix => "_audiodescdiv"}
+    end 
+
+    def update_video_description
+      @literary_quotation = LiteraryQuotation.find(params[:literary_quotation][:id])
+      if @literary_quotation.created_by == nil or @literary_quotation.created_by == ""
+        @literary_quotation.created_by = session[:user].login
+        @literary_quotation.created_at = Time.now
+      end
+      if session[:user] != nil
+        @literary_quotation.updated_by = session[:user].login
+      end
+      @literary_quotation.updated_at = Time.now
+      if @literary_quotation.update_history == nil
+        @literary_quotation.update_history = session[:user].login + " ["+Time.now.to_s+"]
+  "
+      else
+        @literary_quotation.update_history += session[:user].login + " ["+Time.now.to_s+"]
+  "
+      end    
+      respond_to do |format|
+        if @literary_quotation.update_attributes(params[:literary_quotation])
+          format.html do
+            render :partial => 'shared/tinymce_field_show', :locals => {:t => @literary_quotation, :divsuffix => "_videodescdiv"}
+          end
+        else
+          #redirect_to :action => 'index_edit'
+          #redirect_to :action => 'public_edit', :id => @literary_quotation
+        end
+      end
+    end
+
+    def video_description_show
+      @literary_quotation = LiteraryQuotation.find(params[:id])
+      render :partial => "shared/tinymce_field_show", :locals => {:t => @literary_quotation, :divsuffix => "_videodescdiv"}
+    end
+
+    def video_description_edit
+      @literary_quotation = LiteraryQuotation.find(params[:id])
+      render :partial => "shared/tinymce_field_edit", :locals => {:t => @literary_quotation, :divsuffix => "_videodescdiv"}
+    end  
+  
 end
