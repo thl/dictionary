75153fafdf6a225e59834fdd708529fa4a134000
Heiser Mazariegos <hmazariegos@inside-solutions.net>
2011-12-14 19:27:25 -0600

    setting subnavigation home tab to root url

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -319,9 +319,10 @@ module ApplicationHelper
       # The :index values are necessary for this hash's elements to be sorted properly
       if $CURRENT_TERM.nil?
         {
-        #:custom_home => {:index => 1, :title => "Home", :url => "#iframe=#{app_host_url}/reference/dictionaries/tibetan-dictionary/&div_id=universal_navigation_content" },
-        #:custom_home => {:index => 1, :title => "Home", :url => custom_home_path + "#iframe=#{app_host_url}/reference/dictionaries/tibetan-dictionary/&div_id=universal_navigation_content" },
-        :custom_home => {:index => 1, :title => "Home", :url => root_path + "#iframe=#{app_host_url}/reference/dictionaries/tibetan-dictionary/&div_id=universal_navigation_content" },
+        ##:custom_home => {:index => 1, :title => "Home", :url => "#iframe=#{app_host_url}/reference/dictionaries/tibetan-dictionary/&div_id=universal_navigation_content" },
+        ##:custom_home => {:index => 1, :title => "Home", :url => custom_home_path + "#iframe=#{app_host_url}/reference/dictionaries/tibetan-dictionary/&div_id=universal_navigation_content" },
+        #:custom_home => {:index => 1, :title => "Home", :url => root_path + "#iframe=#{app_host_url}/reference/dictionaries/tibetan-dictionary/&div_id=universal_navigation_content" },
+        :custom_home => {:index => 1, :title => "Home", :url => root_path },
         
         #:search => {:index => 2, :title => "Search", :url => root_path },
         :search => {:index => 2, :title => "Search", :url => search_main_definition_path },
@@ -335,9 +336,10 @@ module ApplicationHelper
         }
       else
         {
-        #:custom_home => {:index => 1, :title => "Home", :url => "#iframe=#{app_host_url}/reference/dictionaries/tibetan-dictionary/&div_id=universal_navigation_content" },
-        #:custom_home => {:index => 1, :title => "Home", :url => custom_home_path + "#iframe=#{app_host_url}/reference/dictionaries/tibetan-dictionary/&div_id=universal_navigation_content" },
-        :custom_home => {:index => 1, :title => "Home", :url => root_path + "#iframe=#{app_host_url}/reference/dictionaries/tibetan-dictionary/&div_id=universal_navigation_content" },
+        ##:custom_home => {:index => 1, :title => "Home", :url => "#iframe=#{app_host_url}/reference/dictionaries/tibetan-dictionary/&div_id=universal_navigation_content" },
+        ##:custom_home => {:index => 1, :title => "Home", :url => custom_home_path + "#iframe=#{app_host_url}/reference/dictionaries/tibetan-dictionary/&div_id=universal_navigation_content" },
+        #:custom_home => {:index => 1, :title => "Home", :url => root_path + "#iframe=#{app_host_url}/reference/dictionaries/tibetan-dictionary/&div_id=universal_navigation_content" },
+        :custom_home => {:index => 1, :title => "Home", :url => root_path },
         
         #:search => {:index => 2, :title => "Search", :url => root_path },
         :search => {:index => 2, :title => "Search", :url => search_main_definition_path },
